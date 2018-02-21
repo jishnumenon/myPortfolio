e6a533ccbfebbba7467ca2b2c50a8fb5899ce80b
primestudent <jishnumenona@gmail.com>
2018-02-21 17:55:56 -0500

    php mailer requre class uncommented

--- send.php
+++ send.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['message'])) {
-	//require("class.phpmailer.php"); //you have to download this plugin from github.com - link is in documentation
+	require("class.phpmailer.php"); //you have to download this plugin from github.com - link is in documentation
 	
 	$firstname = 	$_POST['firstname'];
 	$lastname = 	$_POST['lastname'];
